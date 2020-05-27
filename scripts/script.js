62ffc9bb574ecf5719de1e66145722e1655467c9
Monk <djsmilefanatik@yandex.ru>
2020-05-27 20:51:15 +0500

    feature: разделить открытие-закрытие форм

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,67 @@
 const editButton = document.querySelector('.profile__edit-button');
-const closeIcon = document.querySelector('.popup__close-icon');
-const popup = document.querySelector('.popup');
-const form = document.querySelector('.popup__container');
+const closeIcons = document.querySelectorAll('.popup__close-icon');
+const addButton = document.querySelector('.profile__add-button');
+
+const popupEdit = document.querySelector('.popup_type_edit-profile');
+const popupAdd = document.querySelector('.popup_type_add-form');
+const formEdit = document.querySelector('.popup__container_type_edit-profile');
+const formAdd = document.querySelector('.popup__container_type_add-form');
+
 const nameInput = document.querySelector('.popup__input_element_name');
 const jobInput = document.querySelector('.popup__input_element_job');
-    // Элементы, куда должны быть вставлены значения value полей
+    // Элементы, куда должны быть вставлены значения value полей.
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
+
+
+//ФУНКЦИОНАЛ ОТКРЫТИЯ И ЗАКРЫТИЯ ПОПАПА
+//Функция, которая открывает или закрывает Popup.
+function openClose(popup) {
+  popup.classList.toggle('popup_opened');
+}
+
+//Cлушатель на кнопку редактирования.
+editButton.addEventListener('click', function () {
+  openClose(popupEdit);
+//Подтянуть из profileName и profileJob пользователя данные поля формы
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+});
+
+//Cлушатель на кнопку редактирования.
+addButton.addEventListener('click', function () {
+  openClose(popupAdd);
+});
+
+//Cлушатель на крестик.
+closeIcons.forEach(function (icon) {
+  icon.addEventListener('click', function (evt) {
+    const whichPopup = evt.target.closest('div');
+    openClose(whichPopup);
+  });
+});
+
+
+//ФУНКЦИОНАЛ СОХРАНЕНИЯ ФОРМЫ
+// Обработчик «отправки» формы
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы определим свою логику отправки.
+
+    // Получить значение полей из свойства value
+    const nameValue = nameInput.value;
+    const jobValue = jobInput.value;
+
+    // Вставить новые значения с помощью textContent
+    profileName.textContent = nameValue;
+    profileJob.textContent = jobValue;
+
+    //Автоматически закрыть попап
+    popup.classList.remove('popup_opened');
+}
+
+// Прикрепить обработчик к форме:
+formEdit.addEventListener('submit', formSubmitHandler);
 
 
 //НАПОЛНЕНИЕ ELEMENTS 6 КАРТОЧКАМИ
@@ -54,48 +109,3 @@ initialCards.forEach(function (item) {
   //4 отображаем на странице
   elementsList.append(element);
 })
-
-//ФУНКЦИОНАЛ ОТКРЫТИЯ И ЗАКРЫТИЯ ПОПАПА
-//Добавить слушателя на кнопку редактирования.
-editButton.addEventListener('click', openClose);
-
-//Функция, которая открывает Popup.
-function openClose() {
-    //Добавить модификатор.
-  popup.classList.add('popup_opened');
-  //Подтянуть из profileName и profileJob пользователя данные поля формы
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-}
-
-//Добавить слушателя на кнопку закрытия popup.
-closeIcon.addEventListener('click', close);
-
-//Функция, которая закрывает Popup.
-function close() {
-  //Удалить модификатор.
-  popup.classList.remove('popup_opened');
-}
-
-
-
-//ФУНКЦИОНАЛ СОХРАНЕНИЯ ФОРМЫ
-// Обработчик «отправки» формы
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы определим свою логику отправки.
-
-    // Получить значение полей из свойства value
-    const nameValue = nameInput.value;
-    const jobValue = jobInput.value;
-
-    // Вставить новые значения с помощью textContent
-    profileName.textContent = nameValue;
-    profileJob.textContent = jobValue;
-
-    //Автоматически закрыть попап
-    popup.classList.remove('popup_opened');
-}
-
-// Прикрепить обработчик к форме:
-form.addEventListener('submit', formSubmitHandler);
